b5644e81649783f4fb4268ba2d974a6a319b0687
Mukul . <aggarwalmukul96@gmail.com>
2024-10-29 00:40:24 +0530

    added new url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "https://physique-gray.vercel.app/api/v1/";
+// export const BACKEND_URL = "https://physique-gray.vercel.app/api/v1/";
+export const BACKEND_URL = "https://physique-server.onrender.com/api/v1/";
